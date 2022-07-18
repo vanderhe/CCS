ab525705db19de3b8596c90c6265fb3d7c62833c
Tammo van der Heide <vanderhe@uni-bremen.de>
2022-07-18 18:07:50 +0200

    Change DFT format from VASP to NWCHEM

--- ccs/scripts/ccs_build_db.py
+++ ccs/scripts/ccs_build_db.py
@@ -14,7 +14,7 @@ from ase.units import Bohr, Hartree
 
 def ccs_build_db(mode=None, DFT_DB=None, DFTB_DB=None, file_list=None, greedy=False, greed_threshold=0.0001):
 
-    AUtoEvA = Hartree/Bohr
+    AUtoEvA = Hartree / Bohr
 
     if os.path.isfile(DFT_DB):
         print("DFT data-base allready exists. Please delete the file or use another file name.")
@@ -40,49 +40,23 @@ def ccs_build_db(mode=None, DFT_DB=None, DFTB_DB=None, file_list=None, greedy=Fa
     for lns in tqdm(f, total=L):
         counter += 1
         lns = lns.split()
-        DFT_FOLDER = lns[0]
+        DFT_ROOT_FOLDER = lns[0]
+        # DFT_ROOT_FOLDER = os.path.dirname(os.path.abspath(DFT_FOLDER))
 
-        structure_DFT = read(DFT_FOLDER, index=-1)
-        EDFT = structure_DFT.get_potential_energy()
+        structure_DFT = read(os.path.join(DFT_ROOT_FOLDER, 'inp.nw'), index=-1)
+        EDFT = float(np.loadtxt(os.path.join(DFT_ROOT_FOLDER,
+                                             'Total_DFT_Energy_eV')))
+        calculator_DFT = SinglePointCalculator(structure_DFT,
+                                               energy=EDFT,
+                                               free_energy=EDFT)
+        structure_DFT.calc = calculator_DFT
+        structure_DFT.get_potential_energy()
         DFT_DB.write(structure_DFT, PBE=True, key=counter)
 
-        # EXTRACT ALL REASONABLE STEPS?
-        converged_indices = []
-        Natoms = len(structure_DFT)
-        if greedy and (mode != "DFTB"):
-            try:
-                f2 = open(DFT_FOLDER, "r")
-                outcar = f2.read()
-                f2.close
-            except:
-                print("Only implemeted for VASP OUTCAR-files.")
-
-            NELM = int(re.findall("NELM   \=(.+?)\;", outcar)[0])
-            indices = re.findall("Iteration(.+?)\(", outcar)
-            all_energies = re.findall("entropy\=(.+?)e", outcar)
-            all_energies = [float(x)/Natoms for x in all_energies]
-            indices = [int(x)-1 for x in indices]
-            dE = re.findall("2. order\) :(.+?)\(", outcar)
-            uindices = set(indices)
-            converged_indices = []
-            previous_E = EDFT
-            for i in uindices:
-                N_SCF = [(el == i) for el in indices]
-                if(sum(N_SCF) < NELM) & (abs(all_energies[i]-previous_E) > greed_threshold):
-                    converged_indices.append(i)
-                    previous_E = all_energies[i]
-
-            for i in tqdm(converged_indices):
-                counter += 1
-                structure_DFT = read(DFT_FOLDER, index=i)
-                EDFT = structure_DFT.get_potential_energy()
-                DFT_DB.write(structure_DFT, PBE=True, key=counter)
-
         if mode == "DFTB":
-            DFTB_FOLDER = lns[1]
-            # READ DFTB
+            DFTB_ROOT_FOLDER = lns[1]
             structure_DFTB = copy.deepcopy(structure_DFT)
-            f2 = open(DFTB_FOLDER, "r")
+            f2 = open(os.path.join(DFTB_ROOT_FOLDER, 'results.tag'), "r")
             time_to_read = False
             cnt = 0
             while True:
@@ -99,37 +73,41 @@ def ccs_build_db(mode=None, DFT_DB=None, DFTB_DB=None, file_list=None, greedy=Fa
                     break
             f2.close
 
-            # READ DFTB FORCES
-            Natoms = structure_DFTB.get_global_number_of_atoms()
-            time_to_read = False
-            DFTB_forces = np.zeros(
-                [Natoms, 3])
-            while True:
-                next_line = f2.readline()
+            # # READ DFTB FORCES
+            # Natoms = structure_DFTB.get_global_number_of_atoms()
+            # time_to_read = False
+            # DFTB_forces = np.zeros(
+            #     [Natoms, 3])
+            # while True:
+            #     next_line = f2.readline()
 
-                if time_to_read and acnt < Natoms-1:
-                    af = next_line.split()
-                    DFTB_forces[acnt, 0] = float(af[0])*AUtoEvA
-                    DFTB_forces[acnt, 1] = float(af[1])*AUtoEvA
-                    DFTB_forces[acnt, 2] = float(af[2])*AUtoEvA
-                    acnt += 1
+            #     if time_to_read and acnt < Natoms-1:
+            #         af = next_line.split()
+            #         DFTB_forces[acnt, 0] = float(af[0])*AUtoEvA
+            #         DFTB_forces[acnt, 1] = float(af[1])*AUtoEvA
+            #         DFTB_forces[acnt, 2] = float(af[2])*AUtoEvA
+            #         acnt += 1
 
-                if "forces" in next_line:
-                    time_to_read = True
-                    acnt = 0
+            #     if "forces" in next_line:
+            #         time_to_read = True
+            #         acnt = 0
 
-                if not next_line:
-                    break
-            f2.close
+            #     if not next_line:
+            #         break
+            # f2.close
 
-            calculator = SinglePointCalculator(structure_DFTB,
-                                               energy=EDFTB,
-                                               free_energy=EDFTB,
-                                               forces=DFTB_forces)
+            # calculator_DFTB = SinglePointCalculator(structure_DFTB,
+            #                                         energy=EDFTB,
+            #                                         free_energy=EDFTB,
+            #                                         forces=DFTB_forces)
 
-            structure_DFTB.calc = calculator
+            calculator_DFTB = SinglePointCalculator(structure_DFTB,
+                                                    energy=EDFTB,
+                                                    free_energy=EDFTB)
+
+            structure_DFTB.calc = calculator_DFTB
             structure_DFTB.get_potential_energy()
-            structure_DFTB.get_forces()
+            # structure_DFTB.get_forces()
             DFTB_DB.write(structure_DFTB, DFTB=True, key=counter)
 
     f.close()
@@ -147,7 +125,7 @@ def main():
     print("--------------------------------------------------------------------------------")
 
     try:
-        assert sys.argv[1] in ['CCS', 'CCS+Q', 'DFTB'], 'Mode not supproted.'
+        assert sys.argv[1] in ['CCS', 'CCS+Q', 'DFTB'], 'Mode not supported.'
     except:
         exit()
 
@@ -155,14 +133,14 @@ def main():
     file_list = sys.argv[2]
     DFT_data = sys.argv[3]
     print("    Mode: ", mode)
-    if(mode == "CCS" or mode == "CCS+Q"):
+    if mode == "CCS" or mode == "CCS+Q":
         greedy = bool(sys.argv[4])
         print("    DFT data base: ", DFT_data)
         print("    Greedy mode: ", greedy)
         print("")
         print("--------------------------------------------------------------------------------")
         ccs_build_db(mode, DFT_DB=DFT_data, file_list=file_list, greedy=greedy)
-    if(mode == "DFTB"):
+    if mode == "DFTB":
         DFTB_data = sys.argv[4]
         print("    DFT data base: ", DFT_data)
         print("    DFTB data base: ", DFTB_data)
